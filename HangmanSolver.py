33b5839e51ea6e2474bc9e150c98625d627ab97b
JustinStoner1 <jstoner@unm.edu>
2020-01-07 20:06:23 -0700

    Added a word possibility function that takes incorrectly guessed letters into account and renamed functions as needed

--- HangmanSolver.py
+++ HangmanSolver.py
@@ -16,17 +16,34 @@ def loadDictionary(filePath):
     return dictFrame
 
 
-def possibleWords(board, dictionary):
+def getPossibleWordsFromBoard(board, dictionary):
     """
     Uses regular expressions to select all rows of the dictionary dataframe that match the boards size and content
-    :param board: the current state of the hangman game
+    :param board: the current state of the hangman game, used to find size and correct guesses
     :param dictionary: the dictionary the hangman word is believed to be from
     :return: all possible words that could be the secret word bases on the correct guesses and size of the secret word
     """
-    regex = board.replace('_', '.')
-    regex = "\\b\\w{"+str(len(board))+"}\\b"
+    regex = "(?="+board.replace('_', '.')+")(?=\\b\\w{"+str(len(board))+"}\\b)"  # Match words to correct guesses and secret word size
+    return dictionary[dictionary.words.str.match(regex)]
+
+
+def getPossibleWordsFromGame(board, incorrectGuesses, dictionary):
+    """
+    Uses regular expressions to select all rows of the dictionary dataframe that match the boards size and content
+    :param board: the current state of the hangman game, used to find size and correct guesses
+    :param incorrectGuesses: list of guesses letters that are not in the secret word
+    :param dictionary: the dictionary the hangman word is believed to be from
+    :return: all possible words that could be the secret word bases on the correct and incorrect guesses and size of the secret word
+    """
+    # match words to correct guesses and secret word size
     regex = "(?="+board.replace('_', '.')+")(?=\\b\\w{"+str(len(board))+"}\\b)"
-    print("regex: "+regex)
+
+    # ensure words do not contain previous incorrect guesses
+    regex += "(?=^[^"
+    for guess in incorrectGuesses:
+        regex += guess
+    regex += "]*$)"
+
     return dictionary[dictionary.words.str.match(regex)]
 
 
@@ -42,11 +59,22 @@ def rankPossibleGuesses(board, incorrectGuesses, dictionary):
 
 # process arguments
 dictionary2 = pandas.DataFrame(loadDictionary(r"dictionaries/words_alpha.txt"))
-print(dictionary2)
+#print(dictionary2)
+print("loaded", len(dictionary2), "words")
 
-print("zwitterionic")
+print("word: zwitterionic")
 testBoard = "z___________"
-print(testBoard)
-maybies = possibleWords(testBoard, dictionary2)
-print(maybies)
+badGuesses = ['b', 'f', 'h', 'p', 'g']
+print("board:", testBoard)
+print("bad guesses:", badGuesses)
 
+
+heuristic1 = getPossibleWordsFromBoard(testBoard, dictionary2)
+possibilities1 = len(heuristic1)
+print(possibilities1, "w/out incorrect guesses:")
+
+heuristic2 = getPossibleWordsFromGame(testBoard, badGuesses, dictionary2)
+possibilities2 = len(heuristic2)
+print(possibilities2, "w/ incorrect guesses:")
+
+print(possibilities1-possibilities2, "fewer possibilities")
