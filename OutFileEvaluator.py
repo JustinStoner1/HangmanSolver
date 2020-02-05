aa62a42cd7fe64c26100d0307d0db3b46ebfebac
JustinStoner1 <jstoner@unm.edu>
2020-02-04 21:25:56 -0700

    Ran tests for occurrence_Collins Scrabble Words (2019).csv. Added standard deviation to statistics output by OutFileEvaluator.py

--- OutFileEvaluator.py
+++ OutFileEvaluator.py
@@ -5,14 +5,13 @@ def aggregateOutFileData(outFileName: str, aggDataFileName: str):
     with open(outFileName, "r") as outFileCSV:
         outFileReader = csv.DictReader(outFileCSV, delimiter=',')
         entryCount = 0  # len(outFileCSV.readlines())
-        print("aggregating", entryCount, "entries")
         totalWordLength = 0
         totalAvgGuesses = 0
         totalAvgCorrectGuesses = 0
         totalAvgWrongGuesses = 0
         dataDict = {}
         for entry in outFileReader:
-            print(entry)
+            # print(entry)
             entryCount += 1
             wordLength = int(entry["wordLength"])
             guesses = int(entry["guessCount"])
@@ -28,9 +27,9 @@ def aggregateOutFileData(outFileName: str, aggDataFileName: str):
             # specific data
             if wordLength in dataDict:
                 values = dataDict[wordLength]
-                dataDict[wordLength] = (values[0] + guesses, values[1] + correctGuesses, values[2] + wrongGuesses, values[3] + 1)
+                dataDict[wordLength] = (values[0] + guesses, values[1] + guesses*guesses, values[2] + correctGuesses, values[3] + correctGuesses*correctGuesses, values[4] + wrongGuesses, values[5] + wrongGuesses*wrongGuesses, values[6] + 1)
             else:
-                dataDict[wordLength] = (guesses, correctGuesses, wrongGuesses, 1)
+                dataDict[wordLength] = (guesses, guesses*guesses, correctGuesses, correctGuesses*correctGuesses, wrongGuesses, wrongGuesses*wrongGuesses, 1)
 
         avgCorrectGuessesPerLetter = totalAvgGuesses/entryCount  # how many correct guesses it takes to find the secret word per letter in the secret word
         avgGuessesPerLetter = totalAvgCorrectGuesses/entryCount  # how many guesses it takes to find the secret word per letter in the secret word
@@ -40,14 +39,23 @@ def aggregateOutFileData(outFileName: str, aggDataFileName: str):
         print("avgCorrectGuessesPerLetter", avgCorrectGuessesPerLetter)
         print("avgWrongGuessesPerLetter", avgWrongGuessesPerLetter)
 
-    aggDataFileLines = "wordLength,avgGuessesPerLetter,avgCorrectGuessesPerLetter,avgWrongGuessesPerLetter"
+    aggDataFileLines = "wordLength,avgGuessesPerLetter,avgCorrectGuessesPerLetter,avgWrongGuessesPerLetter,stddevGuessesPerLetter,stddevCorrectGuessesPerLetter,stddevWrongGuessesPerLetter"
+    # find mean
+    means = {}
     for k, v in sorted(dataDict.items()):
         # print("\nFor words of length", k)
 
-        avgGuesses = v[0]/v[3]
-        avgCorrectGuesses = v[1]/v[3]
-        avgWrongGuesses = v[2]/v[3]
-        aggDataFileLines += "\n" + str(k) + ',' + str(avgGuesses) + ',' + str(avgCorrectGuesses) + ',' + str(avgWrongGuesses)
+        avgGuesses = v[0]/v[6]
+        avgCorrectGuesses = v[2]/v[6]
+        avgWrongGuesses = v[4]/v[6]
+
+        stddevGuesses = v[1]/v[6]-avgGuesses*avgGuesses
+        stddevCorrectGuesses = v[3]/v[6]-avgCorrectGuesses*avgCorrectGuesses
+        stddevWrongGuesses = v[5]/v[6]-avgWrongGuesses*avgWrongGuesses
+
+        aggDataFileLines += "\n" + str(k) + ',' + str(avgGuesses) + ',' + str(avgCorrectGuesses) + ',' + str(avgWrongGuesses) + ',' + str(stddevGuesses) + ',' + str(stddevCorrectGuesses) + ',' + str(stddevWrongGuesses)
+
+        means[k] = (avgGuesses, avgCorrectGuesses, avgWrongGuesses)
 
     with open(aggDataFileName, "w") as aggDataFile:
         aggDataFile.write(aggDataFileLines)
